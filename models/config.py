945e4fb28c01a5d0e921ced6a00d773bbc4d89b7
muffinh-ub <andrebezerra19099@gmail.com>
2026-01-01 17:30:41 -0300

    v1.3

--- models/config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "app_secret_key": "y71XkvwB2vo8D96Z2Ii9LYj3b8J", #chave secreta session
-    "api_news": "ZgGyYPxbHcuOQu1KmVqR7XiZik2lH6zISvwIe1pJ", #api do marketeux
-    "api_bolsa": "a27Hyjw2VXj2eCjaHTcxGH", # api do brapi
-    "gemini_key": "AIzaSyCrJZrKIDozIT-GlJv_kNlVSZWiYChUA1w", #api do gemini
-    "google_key": "sdix etvd njyc epwm", #senha do google
-    "postgres_key": "3xDjmScAEqXHQS1yR74vcrb0Px09N9y1" #senha do banco de dados
-}
